e3941bc79a02d5f405b4e17e8fa6684a0937a353
emaspero <emaspero@hotmail.com>
2022-06-06 12:58:01 +0100

    Working on adding the wine detail page, populated wine add/index ejs pages

--- controllers/wine.js
+++ controllers/wine.js
@@ -2,6 +2,7 @@
 const {Wine} = require('../models/Wine');
 
 // Require Moment and isLoggedIn below
+const moment = require('moment');
 
 // GET - Load 'Add Wine' form
 exports.wine_create_get = (req, res) => {
@@ -20,5 +21,29 @@ exports.wine_create_post = (req, res) => {
     .catch((err) => {
         console.log(err);
         res.send("Try again later");
+    })
+};
+
+// GET - Wine Index
+exports.wine_index_get = (req, res) => {
+    Wine.find()
+    .then(wines => {
+        res.render('wine/index', {wines, moment})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+// GET - Wine detail page by ID
+exports.wine_show_get = (req, res) => {
+    console.log(req.query.id);
+
+    Wine.findById(req.query.id).populate('review')
+    .then(wine => {
+        res.render('wine/detail', {wine, moment});
+    })
+    .catch((err) => {
+        console.log(err);
     })
 };
